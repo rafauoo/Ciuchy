70dfe53d04f6846f8d15b69287f1ce95a571c4da
rafauoo <rafal5094@gmail.com>
2024-07-22 10:55:12 +0200

    change ip

--- src/App.vue
+++ src/App.vue
@@ -20,7 +20,7 @@ export default {
     updateClothingAction(action) {
       const selectedClothing = this.$refs.clothesCarousel.clothes[this.$refs.clothesCarousel.currentIndex];
       if (selectedClothing) {
-        axios.put(`http://localhost:8000/ciuchy/${selectedClothing.id}/action`, { action: action })
+        axios.put(`http:///20.160.76.93:8000/ciuchy/${selectedClothing.id}/action`, { action: action })
           .then(response => {
             this.$refs.clothesCarousel.clothes[this.$refs.clothesCarousel.currentIndex].action = action;
           })
